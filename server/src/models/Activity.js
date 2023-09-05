2d68b6bf71b09073ff7483d9659b5f744ab4389f
unknown <sachero10@gmail.com>
2023-09-05 11:30:45 -0300

    models

---
+++ server/src/models/Activity.js
@@ -0,0 +1,31 @@
+const {DataTypes} = require("sequelize");
+module.exports = (sequelize) => {
+    sequelize.define(
+        "Activity",
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false, 
+            },
+            difficulty: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            duration: {
+                type: DataTypes.INTEGER,
+            },
+            season: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            timestamps: false,
+        }
+    );
+};
