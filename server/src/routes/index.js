fa312dfe8ba74a2f8b498c42ed9a9d9f35512a91
unknown <sachero10@gmail.com>
2023-09-07 11:46:45 -0300

    createActivity

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const getCountries = require("../controllers/getCountries");
 const getCountryById = require("../controllers/getCountryById");
+const createActivity = require("../controllers/createActivity")
 
 const router = Router();
 
 router.get("/countries", getCountries);
 router.get("/countries/:id", getCountryById);
+router.post("/countries", createActivity);
 
 module.exports = router;
