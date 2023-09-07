6d6c1f46eca1ff0887f499ec26d17d5b025a38ac
Alan Serén Almada <sachero10@gmail.com>
2023-09-06 22:29:28 -0300

    countriesByName

--- server/src/controllers/getCountries.js
+++ server/src/controllers/getCountries.js
@@ -1,26 +1,36 @@
 const axios = require("axios");
+const { Op } = require("sequelize");
 const { Country } = require("../db");
 const { URL_BASE } = process.env;
 
 const getCountries = async (req, res) => {
+  const { countryName } = req.query;
+  console.log(countryName);
   try {
-    const countriesDB = await Country.findAll();
-    if (countriesDB.length == 0) {
-      const { data } = await axios(URL_BASE);
-      for (let country of data) {
-        const newCountry = await Country.create({
-          id: country.cca3,
-          name: country.name.common,
-          flag: country.flags.png,
-          continent: country.region,
-          capital: country.capital,
-          subregion: country.subregion,
-          area: country.area,
-          population: country.population,
-        });
+    if (!countryName) {
+      const countriesDB = await Country.findAll();
+      if (countriesDB.length == 0) {
+        const { data } = await axios(URL_BASE);
+        for (let country of data) {
+          const newCountry = await Country.create({
+            id: country.cca3,
+            name: country.name.common,
+            flag: country.flags.png,
+            continent: country.region,
+            capital: country.capital,
+            subregion: country.subregion,
+            area: country.area,
+            population: country.population,
+          });
+        }
+        const countriesBDD = await Country.findAll();
+        return res.status(200).json(countriesBDD);
       }
-      const countriesBDD = await Country.findAll();
-      return res.status(200).json(countriesBDD);
+    } else {
+      const country = await Country.findAll({
+        where: { name: { [Op.iLike]: `%${countryName}%` } },
+      });
+      return res.status(200).json(country);
     }
   } catch (error) {
     return res.status(500).json({ error: error.message });
--- server/src/controllers/getCountryById.js
+++ server/src/controllers/getCountryById.js
@@ -1,25 +1,22 @@
-const {Activity, Country} = require("../db");
+const { Activity, Country } = require("../db");
 const { URL_BASE } = process.env;
 const axios = require("axios");
 
 const getCountryById = async (req, res) => {
-    const {id} = req.params;
-    console.log(id);
-    try {
-        const country = await Country.findByPk(id);
-        // const country = await Country.findOne({
-        //     // include: {
-        //     //     model: Activity,
-        //     //     attributes: ["name", "difficulty", "duration","season"],
-        //     //     through: {attributes: [],},
-        //     // },
-        //     where: {id:id},
-        // });
-        return res.status(200).json(country);
-    } catch (error) {
+  const { id } = req.params;
+  const idPais = id.toUpperCase();
+  try {
+    const country = await Country.findByPk(idPais, {
+      include: {
+        model: Activity,
+        attributes: ["name", "difficulty", "duration", "season"],
+        through: { attributes: [] }, //para que no muestre nada de la tabla intermedia
+      },
+    });
+    return res.status(200).json(country);
+  } catch (error) {
     return res.status(500).json({ error: error.message });
-        
-    }
-}
+  }
+};
 
 module.exports = getCountryById;
